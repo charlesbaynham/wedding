7071899d22f5444c9d5e029ede91c5d63b61cf6e
Charles Baynham <c.baynham@imperial.ac.uk>
2025-11-15 19:26:37 +0000

    Placeholder

--- flake.nix
+++ flake.nix
@@ -20,17 +20,31 @@
           buildInputs = with pkgs; [ rubyEnv bundler bundix git ];
 
           shellHook = ''
+            # Create serve alias
+            alias serve='bundle exec jekyll serve'
+
             echo "Jekyll wedding website development environment"
             echo ""
             echo "Available commands:"
+            echo "  serve              - Start local development server (alias for bundle exec jekyll serve)"
             echo "  jekyll serve       - Start local development server"
             echo "  jekyll build       - Build static site"
             echo "  bundle install     - Install Ruby dependencies"
             echo "  bundix            - Generate gemset.nix from Gemfile.lock"
             echo ""
-            echo "Get started: bundle install && jekyll serve"
+            echo "Get started: bundle install && serve"
           '';
         };
+
+        # Serve app for running the development server
+        apps.serve = {
+          type = "app";
+          program = toString (pkgs.writeShellScript "serve" ''
+            ${rubyEnv}/bin/bundle exec jekyll serve
+          '');
+        };
+
+        apps.default = self.outputs.apps.${system}.serve;
 
         # Default package builds the site
         packages.default = pkgs.stdenv.mkDerivation {
